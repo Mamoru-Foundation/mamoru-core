6468e589e417a2bd740b6a08fa6acb6e6bed7446
Andrii Sokyrko <wartwvister@gmail.com>
2023-01-02 14:15:27 +0100

    Fix lint

--- mamoru-core/tests/rule/mod.rs
+++ mamoru-core/tests/rule/mod.rs
@@ -1,7 +1,7 @@
 use mamoru_core::Rule;
 
-mod udf;
 mod smoke;
+mod udf;
 
 pub fn active_rule(expression: impl AsRef<str>) -> Rule {
     Rule::new("dummy".to_string(), 0, i64::MAX, expression.as_ref())
